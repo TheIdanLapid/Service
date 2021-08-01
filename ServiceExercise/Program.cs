155954996d6d09ecca84ea99e46a2b6fad7cfd48
TheIdanLapid <eontorch@gmail.com>
2021-08-01 10:20:14 +0300

    Loaded exercise files

---
+++ ServiceExercise/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConnectionPool {
+    public class Program {
+
+        private const int CONNETION_COUNT = 4;
+
+        static void Main(string[] args) {
+            IService service = null; /*** CREATE YOUR SERVICE HERE ***/
+
+            RequestSender requestSender = new RequestSender(service);
+
+            requestSender.sendRequests();
+
+            int result = service.getSummary();
+
+            Console.WriteLine($"Result is {result}");
+        }
+    }
+}
