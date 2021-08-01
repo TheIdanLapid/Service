65bb1f61324fff265070ab2890999502e89079d4
TheIdanLapid <eontorch@gmail.com>
2021-08-01 10:55:21 +0300

    Added RequestHandler class

---
+++ ServiceExercise/RequestHandler.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConnectionPool
+{
+    class RequestHandler : IService
+    {
+        public int getSummary()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void sendRequest(Request request)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
